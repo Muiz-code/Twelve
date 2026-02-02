2f66ebcaf6578fea6bbe46c9dea0b8228e3f1f8b
muiz owolabi <beasteye.official@gmail.com>
2026-02-02 22:27:19 +0100

    Nav fixed

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 z-50 bg-none backdrop-blur-md px-4 md:px-25 md:py-11.5">
+      <nav className="fixed top-0 left-0 right-0 z-50 bg-none backdrop-blur-md px-4 md:px-25 md:py-2.5">
         <div className="mx-auto max-w-7xl ">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
